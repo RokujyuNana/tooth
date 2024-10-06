67771a5aff45275f85cd874c821e47eba42dd5ae
RokujyuNana <81780553+RokujyuNana@users.noreply.github.com>
2024-10-06 20:06:48 +0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,33 @@
 import streamlit as st
-import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
 import numpy as np
 from PIL import Image
+import tensorflow as tf
 
 # Load the quantized TFLite model
-try:
-    interpreter = tf.lite.Interpreter(model_path='tooth_health_model_quant.tflite')
-    interpreter.allocate_tensors()
-except Exception as e:
-    st.error(f"Error loading the model: {e}")
-    st.stop()  # Stop further execution if model loading fails
+interpreter = tf.lite.Interpreter(model_path='dental_health_model_quant.tflite')
+interpreter.allocate_tensors()
 
 # Get input and output tensors
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
 def predict_image(img):
-    # Preprocess the image
-    img = img.resize((150, 150))
-    img_array = np.array(img, dtype=np.float32) / 255.0  # Normalize
+    img_array = image.img_to_array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
     
-    # Set the tensor for input
+    # Set the input tensor
     interpreter.set_tensor(input_details[0]['index'], img_array)
-    interpreter.invoke()  # Run the inference
-
-    # Get the output and return the predicted category
-    prediction = interpreter.get_tensor(output_details[0]['index'])
+    
+    # Run inference
+    interpreter.invoke()
+    
+    # Get output tensor
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    
     categories = ['Disklorasi Gigi', 'Gigi Sehat', 'Karies Gigi', 'Radang Gusi']
-    return categories[np.argmax(prediction)]
+    return categories[np.argmax(output_data)]
 
 # Streamlit interface
 st.title('Dental Health Check')
@@ -37,6 +36,7 @@ uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
 if uploaded_file is not None:
     img = Image.open(uploaded_file)
+    img = img.resize((150, 150))
     st.image(img, caption='Uploaded Image', use_column_width=True)
 
     if st.button('Predict'):
